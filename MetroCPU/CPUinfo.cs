3bed1857f5d19e647e7477bf61302f453cd53f64
Tsung Xu <920114525@qq.com>
2018-11-01 21:24:41 -0700

    添加项目文件。

---
+++ MetroCPU/CPUinfo.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLibSys
+{
+    class CPUinfo
+    {
+        private Ols _ols;
+        public bool LoadSucceeded { get; }
+        public bool SST_support { get; }
+        public uint[,] cpuid = new uint[16, 4];
+        public int MaxCPUIDind { get; }
+        public uint[,] cpuid_ex = new uint[32, 4];
+        public int MaxCPUIDexind { get; }
+        public bool? HWP_support { get; }
+        public readonly string ErrorMessage = "No error";
+
+        public CPUinfo()
+        {
+            _ols = new Ols();
+            uint res = _ols.GetStatus();
+            if (res != 0)
+            {
+                ErrorMessage = ((Ols.Status)res).ToString();
+                LoadSucceeded = false;
+            }
+            else
+            {
+                LoadSucceeded = true;
+                _getCPUID();
+                MaxCPUIDind = (int)cpuid[0, 0];
+                _getCPUIDex();
+                MaxCPUIDexind = (int)(cpuid_ex[0,0]-0x80000000);
+                uint eax = 0, edx = 0;
+                HWP_support = BitsSlicer(cpuid[6, 0], 7, 7) > 0;
+                SST_support = _ols.Rdmsr(0x770, ref eax, ref edx) > 0 ? true : false;
+            }
+        }
+
+        public bool IsCpuid
+        {
+            get
+            {
+                int i = _ols.IsCpuid();
+                return i > 0;
+            }
+        }
+
+        public bool SST_enabled
+        {
+            get
+            {
+                if (SST_support)
+                {
+                    uint eax = 0, edx = 0;
+                    if (_ols.Rdmsr(0x770, ref eax, ref edx) > 0)
+                        return eax > 0 ? true : false;
+                    else
+                        return false;
+                }
+                else
+                    return false;
+            }
+            set
+            {
+                if (value)
+                    _ols.Wrmsr(0x770, 1, 0);
+                else
+                    _ols.Wrmsr(0x770, 0, 0);
+            }
+        }
+
+        public string Manufacturer
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                uint[] ex = new uint[4] { 0, 0, 0, 0 };
+                _ols.Cpuid(0, ref ex[0], ref ex[1], ref ex[2], ref ex[3]);
+                foreach (uint i in new uint[] { ex[1], ex[3], ex[2] })
+                    sb.Append(ExToString(i));
+                return sb.ToString();
+            }
+        }
+
+        public string Vendor
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                uint[] ex = new uint[4] { 0, 0, 0, 0 };
+                foreach (uint ind in new uint[] { 0x80000002, 0x80000003, 0x80000004 })
+                {
+                    _ols.Cpuid(ind, ref ex[0], ref ex[1], ref ex[2], ref ex[3]);
+                    foreach (uint i in ex)
+                        sb.Append(ExToString(i));
+                }
+                return sb.ToString();
+            }
+        }
+
+        private void _getCPUID()
+        {
+            cpuid[0, 0] = 0;
+            cpuid[0, 1] = 0;
+            cpuid[0, 2] = 0;
+            cpuid[0, 3] = 0;
+            if (IsCpuid)
+            {
+                _ols.Cpuid(0, ref cpuid[0, 0], ref cpuid[0, 1], ref cpuid[0, 2], ref cpuid[0, 3]);
+            }
+            int maxind = Math.Min((int)cpuid[0, 0], 0xf);
+            if (maxind > 0)
+            {
+                for (uint tmp = 1; tmp <= maxind; tmp++)
+                {
+                    _ols.Cpuid(tmp, ref cpuid[tmp, 0], ref cpuid[tmp, 1], ref cpuid[tmp, 2], ref cpuid[tmp, 3]);
+                }
+            }
+        }
+
+        private void _getCPUIDex()
+        {
+            cpuid_ex[0, 0] = 0;
+            cpuid_ex[0, 1] = 0;
+            cpuid_ex[0, 2] = 0;
+            cpuid_ex[0, 3] = 0;
+            if (IsCpuid)
+            {
+                _ols.Cpuid(0x80000000, ref cpuid_ex[0, 0], ref cpuid_ex[0, 1], ref cpuid_ex[0, 2], ref cpuid_ex[0, 3]);
+            }
+            int maxind = Math.Min((int)(cpuid_ex[0, 0] - 0x80000000), 0x1f);
+
+            if (maxind > 0)
+            {
+                for (uint tmp = 1; tmp <= maxind; tmp++)
+                {
+                    _ols.Cpuid(tmp+0x80000000, ref cpuid_ex[tmp, 0], ref cpuid_ex[tmp, 1], ref cpuid_ex[tmp, 2], ref cpuid_ex[tmp, 3]);
+                }
+            }
+        }
+
+        public uint BitsSlicer(uint exx, int Highest, int Lowest)
+        {
+            if (Highest < Lowest || Highest > 31 || Highest < 0 || Lowest > 31 || Lowest < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            uint head, tail;
+            tail = exx / (uint)Math.Pow(2, Lowest);
+            head = (uint)(exx / (ulong)Math.Pow(2, Highest + 1) * (ulong)Math.Pow(2, Highest + 1) / (uint)Math.Pow(2, Lowest));
+            return tail - head;
+        }
+
+        public string ToBinary(uint data, int bits)
+        {
+            if (bits > 31 || bits < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return Convert.ToString(data, 2).PadLeft(bits, '0');
+        }
+
+        private static string ExToString(uint i)
+        {
+            byte[] bytes = new byte[4];
+            for (int j = 0; j < 4; j++)
+            {
+                bytes[j] = (byte)(i / Math.Pow(256, j));
+            }
+            return Encoding.ASCII.GetString(bytes);
+        }
+
+
+    }
+}
---
+++ MetroCPU/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using OpenLibSys;
+
+namespace MetroCPU
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    /// 
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            if (!IsAdmin())
+            {
+                RunAsRestart();
+                Environment.Exit(0);
+            }
+            InitializeComponent();
+            cpuinfo = new CPUinfo();
+            if (!cpuinfo.LoadSucceeded)
+            {
+                MessageBox.Show(cpuinfo.ErrorMessage);
+                Environment.Exit(1);
+            }
+            else
+            {
+                MessageBox.Show($"HWP: {cpuinfo.HWP_support}");
+                MessageBox.Show("SST: " + cpuinfo.SST_support.ToString());
+                //MessageBox.Show(cpuinfo.BitsSlicer(0x80001234, 31, 28));
+                Environment.Exit(0);
+                if (!cpuinfo.SST_support)
+                {
+                    SST_TextBlock.Text = "Unavailable";
+                    SST_Dock.IsEnabled = false;
+                }
+                else
+                {
+                    Toggle1.IsChecked = cpuinfo.SST_enabled;
+                }
+            }
+        }
+
+        private CPUinfo cpuinfo;
+
+        private static bool IsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private static bool RunAsRestart()
+        {
+            Process p = Process.GetCurrentProcess();
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                WorkingDirectory = Environment.CurrentDirectory,
+                FileName = p.MainModule.FileName,
+                Verb = "runas"
+            };
+
+            try
+            {
+                Process.Start(startInfo);
+                p.Close();
+                Environment.Exit(0);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void Toggle1_Click(object sender, RoutedEventArgs e)
+        {
+            if (Toggle1.IsChecked.HasValue)
+            {
+                cpuinfo.SST_enabled = Toggle1.IsChecked.Value;
+                Toggle1.IsChecked = cpuinfo.SST_enabled;
+            }
+        }
+    }
+}
