685c9c7d3049ed42111a63dd661a0036b8dae813
Tsung Xu <920114525@qq.com>
2018-11-09 20:03:35 -0800

    logo

--- MetroCPU/WMICPUinfo.cs
+++ MetroCPU/WMICPUinfo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Management;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Management;
 
 namespace OpenLibSys
 {
@@ -14,6 +9,14 @@ namespace OpenLibSys
         public uint MaxClockSpeed { get; private set; }
         public uint NumberOfCores { get; private set; }
         public uint NumberOfLogicalProcessors { get; private set; }
+        public string SocketDesignation { get; private set; }
+        public string Description { get; private set; }
+        public uint L1Cache { get; private set; }
+        public uint L2Cache { get; private set; }
+        public uint L3Cache { get; private set; }
+        public string Family { get; private set; }
+        public string Model { get; private set; }
+        public string Stepping { get; private set; }
 
         public WMICPUinfo()
         {
@@ -21,16 +24,35 @@ namespace OpenLibSys
         }
         public void Refresh()
         {
-            using (ManagementObjectSearcher mos = new ManagementObjectSearcher("SELECT * FROM Win32_Processor"))
+            using (ManagementObject mo = new ManagementObject("Win32_Processor.DeviceID='CPU0'"))
             {
-                foreach (ManagementObject mo in mos.Get())
-                {
-                    Name = (string)mo["Name"];
-                    Manufacturer = (string)mo["Manufacturer"];
-                    MaxClockSpeed = (uint)mo["MaxClockSpeed"];
-                    NumberOfCores = (uint)mo["NumberOfCores"];
-                    NumberOfLogicalProcessors = (uint)mo["NumberOfLogicalProcessors"];
-                }
+                Name = (string)mo["Name"];
+                Manufacturer = (string)mo["Manufacturer"];
+                MaxClockSpeed = (uint)mo["MaxClockSpeed"];
+                NumberOfCores = (uint)mo["NumberOfCores"];
+                NumberOfLogicalProcessors = (uint)mo["NumberOfLogicalProcessors"];
+                SocketDesignation = (string)mo["SocketDesignation"];
+                Description = (string)mo["Description"];
+                int ind1, ind2,ind3,l;
+                ind1 = Description.IndexOf("Family");
+                ind2 = Description.IndexOf("Model");
+                ind3 = Description.IndexOf("Stepping");
+                l = Description.Length;
+                Family = Description.Substring(ind1+7,ind2-ind1-8);
+                Model = Description.Substring(ind2+6,ind3-ind2-7);
+                Stepping = Description.Substring(ind3+9);
+            }
+            using (ManagementObject Mo = new ManagementObject("Win32_CacheMemory.DeviceID='Cache Memory 0'"))
+            {
+                L1Cache = (uint)Mo["InstalledSize"];
+            }
+            using (ManagementObject Mo = new ManagementObject("Win32_CacheMemory.DeviceID='Cache Memory 1'"))
+            {
+                L2Cache = (uint)Mo["InstalledSize"];
+            }
+            using (ManagementObject Mo = new ManagementObject("Win32_CacheMemory.DeviceID='Cache Memory 2'"))
+            {
+                L3Cache = (uint)Mo["InstalledSize"];
             }
         }
     }
